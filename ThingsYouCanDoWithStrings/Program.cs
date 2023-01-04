56209ea61ffeef097c8483e69aea8c9baef638ac
Amy B <110386059+Ab92d@users.noreply.github.com>
2023-01-04 14:05:59 +0000

    Removed Hello World Added new method (UsingArray) and added a call in the main method

--- ThingsYouCanDoWithStrings/Program.cs
+++ ThingsYouCanDoWithStrings/Program.cs
@@ -1,8 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// creating an array
-string[] arr = new string[10];
-arr[0] = "Hello";
-arr[1] = "Mate";
-arr[2] = "!";
-Console.WriteLine(arr[0] + " " + arr[1] + arr[2]);
+﻿using System.Runtime.CompilerServices;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        UsingArray();
+    }
+    private static void UsingArray()
+    {
+        // creating an array
+        string[] arr = new string[10];
+        arr[0] = "Hello";
+        arr[1] = "Mate";
+        arr[2] = "!";
+        Console.WriteLine(arr[0] + " " + arr[1] + arr[2]);
+
+    }
+}
+
+
