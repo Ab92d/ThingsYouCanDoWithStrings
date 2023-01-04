b322b05b3e99dc2c24d01b7b75232d7466527802
Amy B <110386059+Ab92d@users.noreply.github.com>
2023-01-04 13:55:05 +0000

    I have added am array and some data into the array

--- ThingsYouCanDoWithStrings/Program.cs
+++ ThingsYouCanDoWithStrings/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// creating an array
+string[] arr = new string[10];
+arr[0] = "Hello";
+arr[1] = "Mate";
+arr[2] = "!";
+Console.WriteLine(arr[0] + " " + arr[1] + arr[2]);
